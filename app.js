2302cff6dc4b772e0438ae1af6910844d2c8e4a1
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-04-21 11:43:19 -0500

    keyboard funciton added

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// Alphabet array for keyboard
+let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z']
+
 // Categories and Words
 let categories = [['chair', 'bed', 'television', 'couch', 'pillow', 'lamp', 'blanket', 'plant', 'refridgerator', 'microwave', 'dishwasher'], ['tiger','horse', 'lion','monkey', 'giraffe'],['teacher', 'firefighter', 'nurse', 'doctor', 'professor', 'mayor']];
 // Selects an array of words/category and then selects a word
@@ -6,11 +11,13 @@ let selectedCategory = categories[Math.floor(Math.random() * categories.length)]
 let word = selectedCategory[Math.floor(Math.random()* selectedCategory.length)];
     // console.log(word)
 
-    let wordLocation = document.querySelector('#word-guess');
-    let wordSpace = document.createElement('ul')
-    wordSpace.id = 'word'
+   
 
+let wordLocation = document.querySelector('#word-guess');
+let wordSpace = document.createElement('ul')
+wordSpace.id = 'word'
 
+// Creates empty spaces for each character of the word to be displayed on
 function unknownWord(){
     for(let i = 0; i < word.length; i++){
         // console.log(word[i])
@@ -22,4 +29,18 @@ function unknownWord(){
     }
     wordLocation.append(wordSpace);
 }
+// Create and display keyboard
+let keys = document.querySelector('#keyboard')
+
+function keyboard(){
+    chars.forEach((char) =>{
+        let button = document.createElement('button');
+        button.textContent = char;
+        button.value = char;
+        button.type = 'button';
+        button.setAttribute('class', 'btn btn-outline-dark btn-lg')
+        keys.append(button)
+    })
+}
 unknownWord()
+keyboard()
