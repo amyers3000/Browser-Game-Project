2be2cac9b7bc0a8c327c64be7ffa4a153e26b3ba
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-04-21 20:50:28 -0500

    interactiveKeys function made

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// Wrong anwer array
+charUsed = [];
+mistakes = 0;
+
 // Alphabet array for keyboard
 let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z']
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // Categories and Words
 let categories = [['chair', 'bed', 'television', 'couch', 'pillow', 'lamp', 'blanket', 'plant', 'refridgerator', 'microwave', 'dishwasher'], ['tiger','horse', 'lion','monkey', 'giraffe'],['teacher', 'firefighter', 'nurse', 'doctor', 'professor', 'mayor']];
@@ -9,13 +13,14 @@ let categories = [['chair', 'bed', 'television', 'couch', 'pillow', 'lamp', 'bla
 let selectedCategory = categories[Math.floor(Math.random() * categories.length)];
     // console.log(selectedCategory)
 let word = selectedCategory[Math.floor(Math.random()* selectedCategory.length)];
-    // console.log(word)
+    console.log(word)
 
    
 
 let wordLocation = document.querySelector('#word-guess');
-let wordSpace = document.createElement('ul')
-wordSpace.id = 'word'
+let wordSpace = document.createElement('ul');
+wordSpace.setAttribute('class', 'display-3');
+wordSpace.id = 'word';
 
 // Creates empty spaces for each character of the word to be displayed on
 function unknownWord(){
@@ -23,24 +28,53 @@ function unknownWord(){
         // console.log(word[i])
         let space = document.createElement('li');
         space.textContent = "_";
-        space.id = word[i];
+        space.classList.add(word[i]);
         wordSpace.append(space);
         
     }
     wordLocation.append(wordSpace);
+    
+
+    
 }
 // Create and display keyboard
-let keys = document.querySelector('#keyboard')
+let key = document.querySelector('#keyboard');
 
 function keyboard(){
     chars.forEach((char) =>{
         let button = document.createElement('button');
         button.textContent = char;
-        button.value = char;
+        // button.value = char;
         button.type = 'button';
-        button.setAttribute('class', 'btn btn-outline-dark btn-lg')
-        keys.append(button)
+        button.setAttribute('class', 'btn btn-dark btn-lg');
+        button.addEventListener('click', function(){ interactiveKeys(char, button)});
+        key.append(button);
     })
 }
+
+function interactiveKeys(char, button){
+    correctChar = document.getElementsByClassName(char)
+    for(let i=0; i<word.length; i++){
+        if (char === word[i]){
+          for(let j=0; j<correctChar.length; j++){
+              correctChar[j].textContent = char
+          }}
+    }
+    if(word.indexOf(char) === -1){
+        button.setAttribute('disabled', true)
+        charUsed.push(char)
+        mistakes++
+        // Image function
+        
+    }
+ }
+
+
+
+
+    
+
 unknownWord()
 keyboard()
+console.log(charUsed)
+console.log(mistakes)
