b53488359db4733f92f3ee5bd2d071232226907f
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-04-28 18:29:54 -0500

    remove function added

--- app.js
+++ app.js
@@ -5,22 +5,80 @@ mistakes = 0;
 // Alphabet array for keyboard
 let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+'t', 'u', 'v', 'w', 'x', 'y', 'z','-'];
 
 // Categories and Words
-let categories = [['chair', 'bed', 'television', 'couch', 'pillow', 'lamp', 'blanket', 'plant', 'refridgerator', 'microwave', 'dishwasher'], ['tiger','horse', 'lion','monkey', 'giraffe'],['teacher', 'firefighter', 'nurse', 'doctor', 'professor', 'mayor']];
+let categories = [['cyclops','ogre', 'leprechauns', 'gnomes', 'goblins','fairies','gorgon', 'mermaid','phoenix','basilisk'], ['zeus', 'hera', 'poseidon', 'demeter', 'athena', 'apollo', 'artemis'],['mount-olympus', 'troy','underworld', 'athens', 'labryinth']];
+let catNames = ['Mythical Beasts', 'Greek Gods', 'Mythical Places']
 // Selects an array of words/category and then selects a word
-let selectedCategory = categories[Math.floor(Math.random() * categories.length)];
-    // console.log(selectedCategory)
+let selectedCategory = []
+
 let word = selectedCategory[Math.floor(Math.random()* selectedCategory.length)];
     console.log(word)
 
-   
 
+
+// DOM manipulation to make wrong letter section and category buttons
+let side = document.querySelector('aside')
+let wrongDiv = document.createElement('div')
+wrongDiv.id = 'wrong'
+let wrongText = document.createElement('h4')
+wrongDiv.innerHTML = '<h4> Wrong Letters <h4>'
+wrongDiv.append(wrongText)
+side.append(wrongDiv) 
+
+// Adding div that will contain buttons for categories
+let catDiv = document.createElement('div')
+catDiv.innerHTML = '<h3>Select a Category:</h3>'
+catDiv.id = 'categories'
+side.append(catDiv)
+
+// Displaying current category
+let newCat = document.querySelector('#newCat')
+newCat.textContent = 'Category: __'
+
+if(selectedCategory === categories[0]){
+    newCat.textContent = 'Category: ' + catNames[0]
+}else if(selectedCategory===categories[1]){
+    newCat.textContent = 'Category: ' + catNames[1]
+}else if( selectedCategory ===categories[2]){
+    newCat.textContent = 'Category: ' + catNames[2]
+}
+
+
+function catButtons(){
+    for(i=0;i<catNames.length; i++){
+        let button = document.createElement('button')
+        let name = catNames[i]
+        button.textContent = name
+        catDiv.append(button)
+        button.type = 'button'
+        button.setAttribute('class','btn btn-dark btn-lg')
+        button.addEventListener('click', function(){ 
+                if(name === 'Mythical Beasts'){
+                    selectedCategory = categories[0]
+                    newCat.textContent = 'Category: ' + catNames[0]
+                    console.log(selectedCategory)
+                    word = selectedCategory[Math.floor(Math.random()* selectedCategory.length)]
+                    console.log(word)
+                    removeWord()
+                    unknownWord()
+                }
+                
+            
+        })
+        
+    }
+}
+console.log(selectedCategory)
+catButtons()
+
+// DOM manipuation for empty space section
 let wordLocation = document.querySelector('#word-guess');
 let wordSpace = document.createElement('ul');
 wordSpace.setAttribute('class', 'display-3');
 wordSpace.id = 'word';
+wordLocation.append(wordSpace)
 
 // Creates empty spaces for each character of the word to be displayed on
 function unknownWord(){
@@ -32,10 +90,15 @@ function unknownWord(){
         wordSpace.append(space);
         
     }
-    wordLocation.append(wordSpace);
+    wordLocation.append(wordSpace)
     
+}
 
-    
+function removeWord(){
+    while (wordSpace.firstChild){
+        wordSpace.removeChild(wordSpace.firstChild)
+    }
+
 }
 // Create and display keyboard
 let key = document.querySelector('#keyboard');
@@ -52,41 +115,38 @@ function keyboard(){
     })
 }
 
+// Displays letter if correct or stores in the wrong letter div
 function interactiveKeys(char, button){
     correctChar = document.getElementsByClassName(char)
-    for(let i=0; i<word.length; i++){
-        if (char === word[i]){
+        if (word.indexOf(char) >= 0){
           for(let j=0; j<correctChar.length; j++){
               correctChar[j].textContent = char
-          }}
+          }}else if(word.indexOf(char) === -1){
+            button.setAttribute('disabled', true)
+            wrongText.append(' '+char+' ')
+            mistakes++
+            // Image function
     }
-    if(word.indexOf(char) === -1){
-        button.setAttribute('disabled', true)
-        wrongText.append(' '+char+' ')
-        mistakes++
-        // Image function
 
-    }
- }
-
-
-let side = document.querySelector('aside')
-let wrongDiv = document.createElement('div')
-wrongDiv.id = 'wrong'
-let wrongText = document.createElement('h4')
-wrongDiv.innerHTML = '<h3 class> Wrong Letters <h3>'
-wrongDiv.append(wrongText)
-side.append(wrongDiv)
-
-function wrongChar(char){
-    char.forEach((used) =>{
-    wrongDiv.append(used)
-})
 }
+ 
+
+
+
+
+
 
     
 
-unknownWord()
+
 keyboard()
-console.log(charUsed)
-console.log(mistakes)
+
+
+
+if(selectedCategory === categories[0]){
+    newCat.textContent = 'Category: ' + catNames[0]
+}else if(selectedCategory===categories[1]){
+    newCat.textContent = 'Category: ' + catNames[1]
+}else if( selectedCategory ===categories[2]){
+    newCat.textContent = 'Category: ' + catNames[2]
+}
